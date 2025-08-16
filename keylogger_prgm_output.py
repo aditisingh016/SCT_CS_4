61c49bc612bee3918198b0b31e611a78306c95f0
Aditi Kumari <aditi.limitless01@gmail.com>
2025-08-16 19:06:11 +0530

    Add files via upload

---
+++ keylogger_prgm_output.py
@@ -0,0 +1,5 @@
+PS C:\Users\hp5cd\.vscode>
+PS C:\Users\hp5cd\.vscode>  c:; cd 'c:\Users\hp5cd\.vscode'; & 'c:\Users\hp5cd\AppData\Local\Programs\Python\Python313\python.exe' 'c:\Users\hp5cd\.vscode\extensions\ms-python.debugpy-2025.10.0-win32-x64\bundled\libs\debugpy\launcher' '49817' '--' 'C:\Users\hp5cd\.vscode\keylogger.py' 
+PS C:\Users\hp5cd\.vscode> ^C
+PS C:\Users\hp5cd\.vscode>
+PS C:\Users\hp5cd\.vscode>  c:; cd 'c:\Users\hp5cd\.vscode'; & 'c:\Users\hp5cd\AppData\Local\Programs\Python\Python313\python.exe' 'c:\Users\hp5cd\.vscode\extensions\ms-python.debugpy-2025.10.0-win32-x64\bundled\libs\debugpy\launcher' '49930' '--' 'c:\Users\hp5cd\.vscode\keylogger.py' 
